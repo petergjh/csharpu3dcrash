e21d392762950fae8ba0b6ca0e9875d1598c4383
peter <306739889@qq.com>
2020-02-29 07:58:23 +0800

    hello world

---
+++ hello_world/for_loop.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class ForLoop
+    {
+        //求：1到100的累加和
+        public void Test1()
+        {
+            int intResult = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                intResult = intResult + i;
+            }
+            Console.WriteLine("结果是：" + intResult);
+        }
+
+        public static void Main()
+        {
+            ForLoop obj = new Demo1();
+            obj.Test1();
+        }
+    }
+}
